cd5e0613e7477b7b01a783fbed3f6c53ad170de1
Baki <smk633044@gmail.com>
2020-08-17 05:36:07 +0200

    ikinci commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,10 @@ public class FirstClass {
 	public static void main(String[] args) {
 
 		System.out.println("ILk commit from local repository");
+		System.out.println("ikinci commit from local repository");
+		
+		
+		
 		
 	}
 
